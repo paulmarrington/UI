f906948a1e6799b0d40231b76a4252174aafe1ae
paulmarrington <paul@marrington.net>
2018-04-13 11:57:26 +1000

    still undiscovered atlas stuff

--- Assets/Askowl/UI/Scripts/Sprites/Contents.cs
+++ Assets/Askowl/UI/Scripts/Sprites/Contents.cs
@@ -13,17 +13,26 @@
           return sprite.texture; // not in atlas
         }
 
-        int       x       = (int) Math.Ceiling(a: sprite.textureRect.x);
-        int       y       = (int) Math.Ceiling(a: sprite.textureRect.y);
-        int       width   = (int) Math.Ceiling(a: sprite.textureRect.width);
-        int       height  = (int) Math.Ceiling(a: sprite.textureRect.height);
+        int x      = (int) Math.Ceiling(a: sprite.textureRect.x);
+        int y      = (int) Math.Ceiling(a: sprite.textureRect.y);
+        int width  = (int) Math.Ceiling(a: sprite.textureRect.width);
+        int height = (int) Math.Ceiling(a: sprite.textureRect.height);
+
         Texture2D texture = new Texture2D(width: width, height: height);
         texture.SetPixels(sprite.texture.GetPixels(x, y, width, height));
         return texture;
       }
 
       public static Texture2D Texture([NotNull] SpriteAtlas atlas, string name) {
-        return Texture(sprite: atlas.GetSprite(name: name));
+        return Texture(sprite: atlas.GetSprite(name));
+      }
+
+      public static void SetTexture(UITexture uiTexture, SpriteAtlas atlas, string name) {
+        Sprite sprite = atlas.GetSprite(name);
+        if (sprite == null) return;
+
+        uiTexture.mainTexture = sprite.texture;
+        uiTexture.uvRect      = sprite.textureRect;
       }
 
       [NotNull, UsedImplicitly]
